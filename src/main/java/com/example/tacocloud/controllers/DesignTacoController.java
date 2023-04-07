7e0459924eda00566caac6b5ca60d2143b2092c2
Zanggar Zhumagaliyev <megamindinkz@gmail.com>
2023-04-07 17:30:33 +0600

    hello

--- src/main/java/com/example/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/example/tacocloud/controllers/DesignTacoController.java
@@ -9,11 +9,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 import com.example.tacocloud.models.Ingredient;
 import com.example.tacocloud.models.Ingredient.Type;
+
+import javax.validation.Valid;
+
 @Slf4j
 @Controller
 @RequestMapping("/design")
@@ -40,7 +44,21 @@ public class DesignTacoController {
         model.addAttribute("design", new Taco());
         return "design";
     }
-    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type s){
-        return ingredients;
+    @PostMapping
+    public String processDesign(
+            @Valid @ModelAttribute("design") Taco design, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+        // Save the taco design...
+        // We'll do this in chapter 3
+        log.info("Processing design: {}", design);
+
+        return "redirect:/orders/current";
+    }
+
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
     }
 }
